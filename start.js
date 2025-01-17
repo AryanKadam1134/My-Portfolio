77d903d83a0b578d1507fc649c117165fe37ad95
Aryan Kadam <aryankadam3114@gmail.com>
2025-01-17 20:20:46 +0530

    Initial files

---
+++ start.js
@@ -0,0 +1,13 @@
+{
+    const student = {
+        fullName : "Aryan Kadam",
+        
+        age : 20,
+        gender : "Male",
+        phoneNo : 7391949996,
+        isPass : true,
+    };
+
+    
+    console.log(student.fullName);
+}
